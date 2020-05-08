e0c29565c87f831c9572a1fd85da92aa7b33bd33
LaurieBld <48795369+LaurieBld@users.noreply.github.com>
2020-05-08 14:26:59 +0200

    Update user.js
    user signup, hash password with bcrypt

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,4 +1,19 @@
+const bcrypt = require('bcrypt');
+
+const User = require('../models/User');
+
 exports.signup = (req, res, next) => {
+    bcrypt.hash(req.body.password, 10)
+    .then(hash => {
+       const user = new User ({
+          email: req.body.email,
+          password: hash 
+       });
+       user.save()
+       .then(() => res.status(201).json({ message: 'Utilisateur créé !'}))
+       .catch(error => res.status(400).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
 
 };
 
