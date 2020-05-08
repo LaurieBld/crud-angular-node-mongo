06077beab1ab4051ae545ba774650f49839f42de
laurie <laurie.bld@hotmail.fr>
2020-05-08 13:34:35 +0200

    ajout controllers, routers, frontend and backend

---
+++ backend/app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const stuffRoutes = require('./routes/stuff');
+
+
+
+
+mongoose.connect('mongodb+srv://Laurie:Alcatraz31!@cluster0-efymr.mongodb.net/test?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+  
+
+  app.use(bodyParser.json());
+
+  app.use('/api/stuff', stuffRoutes);
+
+
+
+
+
+  module.exports = app;
+
+
+
+
---
+++ backend/routes/stuff.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const stuffCtrl = require('../controllers/stuff');
+
+
+
+router.post('/', stuffCtrl.createThing);
+
+  router.put('/:id', stuffCtrl.modifyThing);
+
+  router.delete('/:id', stuffCtrl.deleteThing);
+
+  router.get('/:id', stuffCtrl.getOneThing);
+
+  router.use('/', stuffCtrl.getAllThings);
+
+  module.exports = router;
