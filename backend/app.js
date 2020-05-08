f9209b0a2bc6950f6980eb61ed23bd00f60f83d6
LaurieBld <48795369+LaurieBld@users.noreply.github.com>
2020-05-08 14:14:40 +0200

    Update app.js
    update app js, enregistrement router user

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
 
 
 
@@ -26,6 +27,7 @@ app.use((req, res, next) => {
   app.use(bodyParser.json());
 
   app.use('/api/stuff', stuffRoutes);
+ app.use('/api/auth', userRoutes);
 
 
 
