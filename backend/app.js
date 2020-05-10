539d6442db0e881f83998fc89226869442432b1a
LaurieBld <48795369+LaurieBld@users.noreply.github.com>
2020-05-10 18:38:31 +0200

    Update app.js
    add app.use images

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const stuffRoutes = require('./routes/stuff');
 const userRoutes = require('./routes/user');
@@ -25,6 +26,7 @@ app.use((req, res, next) => {
   
 
   app.use(bodyParser.json());
+  app.use('/images', express.static(path.join(__dirname, 'images')));
 
   app.use('/api/stuff', stuffRoutes);
  app.use('/api/auth', userRoutes);
