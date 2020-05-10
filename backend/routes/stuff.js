574df90ef770bdeac6cbc95cc8054046962bbbc7
LaurieBld <48795369+LaurieBld@users.noreply.github.com>
2020-05-10 18:35:23 +0200

    Update stuff.js
    update with multer

--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const stuffCtrl = require('../controllers/stuff');
 const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 
 
 
-router.post('/', auth, stuffCtrl.createThing);
+router.post('/', auth, multer, stuffCtrl.createThing);
 
   router.put('/:id', auth, stuffCtrl.modifyThing);
 
